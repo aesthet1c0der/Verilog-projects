f6d5ce3b697032d762949ec48dfc54452800c8da
aesthet1c0der <113450738+aesthet1c0der@users.noreply.github.com>
2022-12-27 01:38:12 +0530

    Add files via upload

---
+++ 1-bit half adder/HAS_testbench.v
@@ -0,0 +1,31 @@
+module testbench();
+  
+    // Declare variables and nets for module ports
+    reg a;
+    reg b;
+    wire sum;
+    wire carry;  
+  
+    // Instantiate the module
+    half_adder_structural HALF_ADD(
+        .a(a),
+        .b(b),
+        .sum(sum),
+        .carry(carry)
+        );
+  
+    // Generate stimulus and monitor module ports
+    initial begin
+        $monitor("a=%b, b=%b, sum=%b, carry=%b", a, b, sum, carry);
+    end  
+  
+    initial begin
+        #1; a = 0; b = 0;
+        #1; a = 0; b = 1;
+        #1; a = 1; b = 0;
+        #1; a = 1; b = 1;
+        #1; a = 0; b = 0;
+        #1; a = 1; b = 1;
+    end
+  
+endmodule
